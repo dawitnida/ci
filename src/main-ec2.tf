aca6c26f3950fb34687c82faaeb674f8d70f4b30
Dawit Nida <dawitnida@users.noreply.github.com>
2020-02-28 16:23:30 +0200

    Create main-ec2.tf

---
+++ src/main-ec2.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  profile    = "dev"
+  region     = "eu-west-1"
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ubuntu-test-jenkins" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu-test-jenkins.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    AccountId   = "${data.aws_caller_identity.current.account_id}"
+    Environment = "stage"
+    Profile     = "dev"
+  }
+}
+
+output "ip" {
+  value = "${aws_instance.web.public_ip}"
+}
